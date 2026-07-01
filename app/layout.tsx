18d34f78e76fda5fb86008d0f0b54405b9a6feed
thuanbuiit <thuanbuivan2004@gmail.com>
2026-07-01 22:40:33 +0700

    chore: update metadata for wedding invitation and add featured image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,34 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+    metadataBase: new URL(
+        "https://wedding-invitation-is5acftca-thuan-buis-projects.vercel.app",
+    ),
     title: "Long Vũ & Thu Hiền | Thiệp Cưới",
     description:
         "Thiệp cưới online của Long Vũ & Thu Hiền - Trân trọng kính mời quý khách đến chung vui cùng gia đình chúng tôi.",
+    openGraph: {
+        title: "Thiệp cưới Long Vũ & Thu Hiền",
+        description:
+            "Trân trọng kính mời bạn đến chung vui cùng gia đình chúng tôi.",
+        url: "/",
+        type: "website",
+        images: [
+            {
+                url: "/images/aVUQ_8724.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Thiệp cưới Long Vũ & Thu Hiền",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Thiệp cưới Long Vũ & Thu Hiền",
+        description:
+            "Trân trọng kính mời bạn đến chung vui cùng gia đình chúng tôi.",
+        images: ["/images/hero.jpg"],
+    },
 };
 
 export const viewport = {
